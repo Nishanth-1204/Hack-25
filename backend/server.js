575d973027d0f7ecaee3f682d054a7b3e41c086c
Nishanth <itznishanth250@gmail.com>
2025-02-19 21:46:00 +0530

    updated deploy v5

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,
+  })
   .then(() => {
     console.log("Connected to DB");
   })
