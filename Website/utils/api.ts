13de9108ce885af713443217972866b0e3eb709a
Nishanth <itznishanth250@gmail.com>
2025-02-19 19:16:17 +0530

    Deploy v2

--- Website/utils/api.ts
+++ Website/utils/api.ts
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const api = axios.create({
     baseURL: "https://hack-25-backend-ca2a6k7zh-nishanth-bots-projects.vercel.app/api/v1",
-    
+    withCredentials: true 
 })
