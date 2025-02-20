51b5624e1209c8b15fdcca2132c2700226ca2bbc
Nishanth <itznishanth250@gmail.com>
2025-02-20 10:40:14 +0530

    Update Registration.tsx

--- Website/src/pages/Registration/Registration.tsx
+++ Website/src/pages/Registration/Registration.tsx
@@ -64,8 +64,7 @@ const Register: React.FC = () => {
             alert("Registration successful");
           });
       } catch (error: any) {
-        const errMsg = error.response;
-        alert(errMsg);
+        console.log(error);
       }
     } else {
       alert("You have already registered!");
