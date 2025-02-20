01a2466f5e0beb8fb852f1e613b6c60be698fe7b
Nishanth <itznishanth250@gmail.com>
2025-02-20 12:34:33 +0530

    final deploy v4

--- Website/src/pages/Registration/Registration.tsx
+++ Website/src/pages/Registration/Registration.tsx
@@ -130,7 +130,7 @@ const Register: React.FC = () => {
               required
             >
               <option value="">Select</option>
-              {[1, 3, 4, 5, 6].map((num) => (
+              {[3, 4, 5, 6].map((num) => (
                 <option key={num} value={num}>
                   {num}
                 </option>
